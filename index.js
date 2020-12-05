e16c5e7785266d3e501800358fddac19078d71fd
Abdulrahman Ghalayini <ghalayini.abdulrahman.17@gmail.com>
2020-12-05 13:25:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let main = document.getElementById("main");
 main.remove();
 var btn = document.createElement("h1");
-btn.set
+btn.id="victory"
 let newHeader = document.getElementById("h1#victory");
